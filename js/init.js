5848506bc5af96adfa6f5661744906ec057b884a
Jia <71865277+jangjia01234@users.noreply.github.com>
2023-02-03 03:52:07 +0900

    :recycle: 불필요한 코드 삭제, 전체 리팩토링

--- js/init.js
+++ js/init.js
@@ -1,1 +1,20 @@
 // 여기에 공통 코드 작성
+
+const timerElement = document.createElement("span");
+const barElement = document.querySelector(".bar_upside_left");
+timerElement.className = "timer";
+
+// timerElement.innerText = 4;
+
+barElement.appendChild(timerElement);
+
+let time = 30;
+
+let timeCounter = setInterval(function () {
+  timerElement.innerText = time--;
+
+  if (time < 0) {
+    clearInterval(timeCounter);
+    document.querySelector(".timer").innerHTML = "시간초과";
+  }
+}, 1000);
