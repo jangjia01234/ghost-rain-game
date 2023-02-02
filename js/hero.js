8fd5aee5b9290716aabe4f7ef5258ff213dde329
Jia <71865277+jangjia01234@users.noreply.github.com>
2023-02-02 18:04:02 +0900

    :construction: 용사 좌우 이동 구현

--- js/hero.js
+++ js/hero.js
@@ -2,8 +2,8 @@ const heroElement = document.createElement("div");
 const bgElement = document.querySelector(".bg_image");
 
 heroElement.style.position = "absolute";
-heroElement.style.top = "0";
-heroElement.style.left = "0";
+heroElement.style.bottom = "0";
+heroElement.style.left = "48%"; // 35px 더 가 있으니.. 50%는 조금 오른쪽인듯
 
 heroElement.style.width = HERO_WIDTH + "px";
 heroElement.style.height = HERO_HEIGHT + "px";
@@ -14,38 +14,46 @@ bgElement.appendChild(heroElement);
 document.addEventListener("keydown", function (e) {
   let key = e.key || e.keyCode;
 
+  let heroLeft = getComputedStyle(heroElement).left;
+  let heroLeftNum = heroLeft.split("px")[0];
+
   if (key === "ArrowLeft" || key === 37) {
     heroElement.style.backgroundPosition = "-70px";
+    heroElement.style.left = heroLeftNum - 10 + "px";
   } else if (key === "ArrowRight" || key === 39) {
     heroElement.style.backgroundPosition = "-105px";
-  } else if (key === "ArrowDown" || key === 30) {
-    heroElement.style.backgroundPosition = "-35px";
-  } else {
-    heroElement.style.backgroundPosition = "0";
+    heroElement.style.left = heroLeftNum + 10 + "px";
   }
 });
 
-// const heroElement = document.getElementsByClassName("hero")[0];
+document.addEventListener("keyup", function () {
+  heroElement.style.backgroundPosition = "0";
+});
+
+//// 상하좌우 모두 움직이는 버전 (캐릭터 모습 변경 + 위치 이동) -> 에러 조금 있음
 
 // document.addEventListener("keydown", function (e) {
-//   //   const heroLeft = getComputedStyle(heroElement).left;
-//   //   const heroLeftNum = heroLeft.split("px")[0];
+//     let key = e.key || e.keyCode;
 
-//   let key = e.key || e.keyCode;
+//     let heroLeft = getComputedStyle(heroElement).left;
+//     let heroLeftNum = heroLeft.split("px")[0];
 
-//   if (key === "Left" || key === 37) {
-//     // heroElement.style.left = heroLeftNum - 1 + "px";
-//     // heroElement.className = "left";
-//     console.log("left");
-//   } else if (key === "Right" || key === 39) {
-//     // heroElement.style.left = heroLeftNum + 1 + "px";
-//     // heroElement.className = "right";
-//     console.log("right");
-//   } else {
-//     console.log("error occured!");
-//   }
-// });
+//     let heroTop = getComputedStyle(heroElement).top;
+//     let heroTopNum = heroTop.split("px")[0];
 
-// document.addEventListener("keyup", function () {
-//   heroElement.className = "stop";
+//     if (key === "ArrowLeft" || key === 37) {
+//       heroElement.style.backgroundPosition = "-70px";
+//       heroElement.style.left = heroLeftNum - 4 + "px";
+//     } else if (key === "ArrowRight" || key === 39) {
+//       heroElement.style.backgroundPosition = "-105px";
+//       heroElement.style.left = heroLeftNum + 4 + "px";
+//     } else if (key === "ArrowUp" || key === 38) {
+//       heroElement.style.backgroundPosition = "0";
+//       heroElement.style.top = heroTopNum - 4 + "px";
+//     } else if (key === "ArrowDown" || key === 30) {
+//       heroElement.style.backgroundPosition = "-35px";
+//       heroElement.style.top = heroTopNum + 4 + "px";
+//     } else {
+//       heroElement.style.backgroundPosition = "0";
+//     }
 // });
