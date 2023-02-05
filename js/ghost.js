6ddd27330864e9bec674dad4aea4dd73a1c22ad0
Jia <71865277+jangjia01234@users.noreply.github.com>
2023-02-05 21:02:44 +0900

    :sparkles: 유령폭우 내리기 구현

--- js/ghost.js
+++ js/ghost.js
@@ -5,7 +5,7 @@ function createGhost() {
 
   ghostElement.style.position = "absolute";
   ghostElement.style.top = "0px";
-  ghostElement.style.left = randomNum() + "px";
+  ghostElement.style.left = randomLeftNum() + "px";
 
   ghostElement.style.width = GHOST_WIDTH + "px";
   ghostElement.style.height = GHOST_HEIGHT + "px";
@@ -31,7 +31,65 @@ function createGhost() {
 
 createGhost();
 
-function randomNum() {
+function createAnotherGhost() {
+  const newGhostElement = document.createElement("div");
+  newGhostElement.className = "ghost_element";
+  const newBgElementGhost = document.querySelector(".screen");
+
+  newGhostElement.style.position = "absolute";
+  newGhostElement.style.top = "-40px";
+  newGhostElement.style.left = randomLeftNum() + "px";
+
+  newGhostElement.style.width = GHOST_WIDTH + "px";
+  newGhostElement.style.height = GHOST_HEIGHT + "px";
+  newGhostElement.style.background = 'url("./images/ghost.png") no-repeat';
+
+  newBgElementGhost.appendChild(newGhostElement);
+
+  setInterval(function () {
+    let newGhostTopNum = Number(newGhostElement.style.top.split("px")[0]) + 10;
+    if (newGhostTopNum > BG_HEIGHT - GHOST_HEIGHT + 10) {
+      return;
+    }
+    newGhostElement.style.top = newGhostTopNum + "px";
+  }, 100);
+}
+
+createAnotherGhost();
+
+function createLastGhost() {
+  const lastGhostElement = document.createElement("div");
+  lastGhostElement.className = "ghost_element";
+  const lastBgElementGhost = document.querySelector(".screen");
+
+  lastGhostElement.style.position = "absolute";
+  lastGhostElement.style.top = "-80px";
+  lastGhostElement.style.left = randomLeftNum() + "px";
+
+  lastGhostElement.style.width = GHOST_WIDTH + "px";
+  lastGhostElement.style.height = GHOST_HEIGHT + "px";
+  lastGhostElement.style.background = 'url("./images/ghost.png") no-repeat';
+
+  lastBgElementGhost.appendChild(lastGhostElement);
+
+  setInterval(function () {
+    let lastGhostTopNum =
+      Number(lastGhostElement.style.top.split("px")[0]) + 10;
+    if (lastGhostTopNum > BG_HEIGHT - GHOST_HEIGHT + 10) {
+      return;
+    }
+    lastGhostElement.style.top = lastGhostTopNum + "px";
+  }, 100);
+}
+
+createLastGhost();
+
+function randomLeftNum() {
   let randomLeft = Math.floor(Math.random() * (BG_WIDTH - GHOST_WIDTH));
   return randomLeft;
+}
+
+function randomTopNum() {
+  let randomTop = Math.floor(Math.random() * (BG_HEIGHT - GHOST_HEIGHT));
+  return randomTop;
 }
