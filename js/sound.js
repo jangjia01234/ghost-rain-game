94612491d75bc8ce96326c101e77e66cc687ff3d
Jia <71865277+jangjia01234@users.noreply.github.com>
2023-02-06 00:12:17 +0900

    :adhesive_bandage: 음악 재생 액션 수정

--- js/sound.js
+++ js/sound.js
@@ -1,18 +1,17 @@
-// const audioContainer = document.querySelector(".audioContainer");
-var audio = new Audio("./audio/Arcade_Kid.mp3");
-audio.loop = true;
-audio.volume = 0.5;
-audio.play();
-
 let audioOffBtn = document.querySelector(".icon_volume_off");
-
 let audioOnBtn = document.querySelector(".icon_volume_on");
 
-audioOffBtn.addEventListener("click", function () {
-  audio.pause();
-  audioOffBtn.style.display = "none";
-  audioOnBtn.style.display = "block";
-  audioOnBtn.addEventListener("click", function () {
-    audio.play();
+audioOnBtn.addEventListener("click", function () {
+  var audio = new Audio("./audio/Arcade_Kid.mp3");
+  audio.loop = true;
+  audio.volume = 0.5;
+  audio.play();
+  audioOnBtn.style.display = "none";
+  audioOffBtn.style.display = "block";
+
+  audioOffBtn.addEventListener("click", function () {
+    audio.volume = 0;
+    audioOffBtn.style.display = "none";
+    audioOnBtn.style.display = "block";
   });
 });
