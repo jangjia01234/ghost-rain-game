32b8663d3f93a82b4e476db166c48ced18d89e59
Jia <71865277+jangjia01234@users.noreply.github.com>
2023-02-06 16:28:09 +0900

    :recycle: css -> js 리팩토링

--- js/init.js
+++ js/init.js
@@ -1,5 +1,10 @@
 // 여기에 공통 코드 작성
 
+const htmlElement = document.querySelector("html");
+htmlElement.style.boxSizing = "border-box";
+htmlElement.style.fontFamily = "Silkscreen, cursive";
+
+// timer
 const timerElement = document.createElement("span");
 const barElement = document.querySelector(".bar_upside_left");
 timerElement.className = "timer";
@@ -23,9 +28,6 @@ let timeCounter = setInterval(function () {
 }, 1000);
 
 // to do list (0203 금요일 기준)
-// 3. 상단바 디자인 수정 (목숨, 볼륨 버튼)
 // 4. 시작 화면 구현 (press start) -> 화면 사라지면서 타이머 시작
-// 5. 오디오 컨트롤 확인 (누르면 켜도록 js로 수정), 아이콘 변경,
-// 노래도 바꿀까..? 일단 소리도 너무 크다
 // 6. (선택) 유령 죽일 때마다 상단바 숫자 증가
 // 7. (선택) 유령에 닿을 때마다 하트 하나씩 감소
